e2e747c35121895063c15d7b4c2d09e0e25dfcef
Gius. Camerlingo <gcamerli@protonmail.com>
2018-02-15 16:00:41 +0100

    Clean post install | Fix nvm settings

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # ./post_install.sh
 
-# Set nvm
-export NVM_DIR=$HOME/.nvm
+# Set nvm version
 export NODE_VERSION=8.2.0
 
 # Set bitcore version
-export BITCORE_VERSION=4.1.1
-export BITCORE_BETA=5.0.0-beta.44
+export BITCORE_CPP=4.1.1
+export BITCORE_JS=5.0.0-beta.44
 
 # Load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
@@ -16,15 +15,8 @@ export BITCORE_BETA=5.0.0-beta.44
 nvm install $NODE_VERSION
 
 # Install bitcore
-npm install -g bitcore@$BITCORE_BETA
+npm install -g bitcore@$BITCORE_JS
 
 # Create first node
 bitcore create testnode
 cd testnode
-
-# Install insight modules
-#bitcore install insight-api
-#bitcore install insight-ui
-
-# Launch bitcored
-bitcored
