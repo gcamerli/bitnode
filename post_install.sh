d6a8dfec1b44dbf5ab2be4a2d7e57625ed02b1cc
Gius. Camerlingo <gcamerli@protonmail.com>
2018-02-15 12:17:59 +0100

    Update post install

--- post_install.sh
+++ post_install.sh
@@ -3,22 +3,28 @@
 
 # Set nvm
 export NVM_DIR=$HOME/.nvm
-export NODE_VERSION=4.5.0
+export NODE_VERSION=8.2.0
 
-#Load nvm
+# Set bitcore version
+export BITCORE_VERSION=4.1.1
+export BITCORE_BETA=5.0.0-beta.44
+
+# Load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
 
 # Download nvm version
 nvm install $NODE_VERSION
 
 # Install bitcore
-npm install -g bitcore
+npm install -g bitcore@$BITCORE_BETA
 
 # Create first node
 bitcore create testnode
 cd testnode
-bitcore install insight-api
-bitcore install insight-ui
 
-# Open zsh
-zsh
+# Install insight modules
+#bitcore install insight-api
+#bitcore install insight-ui
+
+# Launch bitcored
+#bitcored
