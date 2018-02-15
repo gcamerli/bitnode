a2193e49e1fa6476d5aa29e9a03f72b1e04b2ba0
Gius. Camerlingo <gcamerli@protonmail.com>
2018-02-15 12:22:03 +0100

    Update post install for creativechain

--- post_install.sh
+++ post_install.sh
@@ -5,10 +5,6 @@
 export NVM_DIR=$HOME/.nvm
 export NODE_VERSION=8.2.0
 
-# Set bitcore version
-export BITCORE_VERSION=4.1.1
-export BITCORE_BETA=5.0.0-beta.44
-
 # Load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
 
@@ -16,15 +12,15 @@ export BITCORE_BETA=5.0.0-beta.44
 nvm install $NODE_VERSION
 
 # Install bitcore
-npm install -g bitcore@$BITCORE_BETA
+npm install -g creacore
 
 # Create first node
-bitcore create testnode
+creacore create testnode
 cd testnode
 
 # Install insight modules
-#bitcore install insight-api
-#bitcore install insight-ui
+#creacore install insight-crea-api
+#creacore install insight-crea-ui
 
 # Launch bitcored
-#bitcored
+#creacored
