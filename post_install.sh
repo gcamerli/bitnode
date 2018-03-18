70502424c12a50e7e8c054f2b0b1e4bde255a91a
Gius. Camerlingo <gcamerli@protonmail.com>
2018-03-18 18:37:42 +0100

    Update post install

--- post_install.sh
+++ post_install.sh
@@ -4,18 +4,14 @@
 # Set nvm version
 export NODE_VERSION=8.2.0
 
-# Set bitcore version
-export BITCORE_CPP=4.1.1
-export BITCORE_JS=5.0.0-beta.44
-
 # Load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
 
 # Download nvm version
 nvm install $NODE_VERSION
 
-# Install bitcore
-npm install -g bitcore@$BITCORE_JS
+# Install bitcore beta
+npm install -g bitcore@beta
 
 # Create first node
 bitcore create testnode
